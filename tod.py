19acfc9d00d65ef1ed78523bbafad899fd1f8f63
ntmrkx <160361922+ntmrkx@users.noreply.github.com>
2024-02-18 21:55:07 +0100

    Update tod.py

--- tod.py
+++ tod.py
@@ -1,60 +1,74 @@
 import tkinter as tk
 
+
 def add_task():
     task = main_task.get()
     if task:
         listbox_tasks.insert(tk.END, task)
         main_task.delete(0, tk.END)
     else:
-        lable_2.config(text = 'Enter a task')
+        lable_2.config(text='Enter a task')
+
 
 def remove_task():
     try:
         index = listbox_tasks.curselection()[0]
         listbox_tasks.delete(index)
     except IndexError:
-        lable_2.config(text = 'Task isn\'t selected.')
+        lable_2.config(text='Task isn\'t selected.')
 
 
 win = tk.Tk()
 photo = tk.PhotoImage(file='todo.png')
 win.iconphoto(False, photo)
-win.config(bg= '#C08EFA')
+win.config(bg='#AFA9A9')
 win.title('To-Do App')
 win.geometry('500x600+100+200')
 win.resizable(True, True)
 win.minsize(300, 400)
 win.maxsize(700, 800)
 
-
 main_task = tk.Entry(win, width=40)
 
-
 lable_1 = tk.Label(win, text='Your To-Do List',
-                   bg='#C08EFA',
+                   bg='#AFA9A9',
                    fg='black',
-                   font=('Arial',10,'bold'),
+                   font=('Arial', 10, 'bold'),
                    width=25,
                    height=2,
                    anchor='n',
                    )
 
-
-lable_2 = tk.Label(win, text = '',
-                   fg='cyan',
+lable_2 = tk.Label(win, text='',
+                   fg='red',
+                   bg='#AFA9A9'
                    )
 
-
-btn1 = tk.Button(win, text= 'Add Task',
-                 width = 15,
-                 command = add_task
+btn1 = tk.Button(win, text='Add Task',
+                 width=15,
+                 command=add_task
                  )
 
-
-btn2 = tk.Button(win, text = 'Remove Task',
-                 width = 15,
-                 command = remove_task
+btn2 = tk.Button(win, text='Remove Task',
+                 width=15,
+                 command=remove_task
                  )
+
+listbox_tasks = tk.Listbox(win, width=50)
+
+lable_1.pack()
+
+main_task.pack()
+
+btn1.pack()
+
+listbox_tasks.pack()
+
+btn2.pack()
+
+lable_2.pack()
+
+win.mainloop()
 
 
 listbox_tasks = tk.Listbox(win, width = 50)
