bf3eae29b61475ffdde65c17fba64d297773bd1d
ntmrkx <160361922+ntmrkx@users.noreply.github.com>
2024-02-18 21:08:22 +0100

    Add files via upload

---
+++ tod.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+
+def add_task(task):
+    task = main_task.get()
+    if task:
+        listbox_tasks.insert(tk.END, task)
+        main_task.delete(0, tk.END)
+    else:
+        label_2.config(text = 'Enter a task')
+
+def remove_task(task):
+    try:
+        index = listbox_tasks.curselection()[0]
+        listbox_tasks.delete(index)
+    except IndexError:
+        label_2.config(text = 'Task isn\'t selected.')
+
+
+win = tk.Tk()
+photo = tk.PhotoImage(file='idk.png')
+win.iconphoto(False, photo)
+win.config(bg= '#C08EFA')
+win.title('To-Do App')
+win.geometry('500x600+100+200')
+win.resizable(True, True)
+win.minsize(300, 400)
+win.maxsize(700, 800)
+
+
+main_task = tk.Entry(win, width=40)
+
+
+lable_1 = tk.Label(win, text='Your To-Do List',
+                   bg='#C08EFA',
+                   fg='black',
+                   font=('Arial',10,'bold'),
+                   width=25,
+                   height=3,
+                   anchor='n',
+                   )
+
+
+lable_2 = tk.Label(win, text = '',
+                   fg='cyan'
+                   )
+
+
+
+btn1 = tk.Button(win, text= 'Add Task',
+                 width = 15,
+                 command = add_task
+                 )
+btn1.grid(row=0, column=1, padx=5, pady=5)
+
+
+btn2 = tk.Button(win, text = 'Remove Task',
+                 width = 15,
+                 command = remove_task
+                 )
+btn2.grid(row=2, column=0, padx=5, pady=5)
+
+
+listbox_tasks = tk.Listbox(win, width = 50)
+listbox_tasks.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+
+btn1.pack()
+btn2.pack()
+lable_1.pack()
+lable_2.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+main_task.pack()
+
+
+win.mainloop()
